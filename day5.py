f15e703f2fa463fb4dfbba7bbbf07160f2ee2ebe
Deekshavanga <sweetydeeksha617@gmail.com>
2025-10-06 18:15:45 +0530

    Update day5.py

--- day5.py
+++ day5.py
@@ -8,3 +8,15 @@ class person:
       
 person= person("Akhil",23)
 print(person)
+
+
+class dog:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+    
+    def __str__(self):
+      return f"{self.name},{self.age} years old"
+      
+dog= dog("Jimmy",2)
+print(dog)
