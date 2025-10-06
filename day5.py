357cbacb2a0e502c78bdae2911f063a503597c5e
Deekshavanga <sweetydeeksha617@gmail.com>
2025-10-06 18:00:40 +0530

    Update day5.py

--- day5.py
+++ day5.py
@@ -1,1 +1,10 @@
-
+class person:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+    
+    def __str__(self):
+      return f"{self.name},{self.age} years old"
+      
+person= person("Akhil",23)
+print(person)
